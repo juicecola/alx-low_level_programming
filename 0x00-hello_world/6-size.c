75e17a44444304b4a694310a9f9ca1d4179bbc91
Eric <irerieric@gmail.com>
2022-09-08 14:40:29 -0700

    prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ char charType;
 long long longlongType;
 printf("Size of int: %zu byte\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of long: %zu bytes\n", sizeof(longType));
+printf("Size of long int: %zu bytes\n", sizeof(longType));
 printf("Size of char: %zu byte\n", sizeof(charType));
 printf("Size of long long: %zu byte\n", sizeof(longlongType));
 return (0);
