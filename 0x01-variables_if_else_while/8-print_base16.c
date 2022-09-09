110a2ae6d4e831038efbcfe3ad5548e11379fbf5
Eric <irerieric@gmail.com>
2022-09-09 13:14:43 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+	
+        for (c = 'a'; c <= 'f'; c++)
+	        putchar(c);
+	putchar('\n');
+
+	return (0);
+}
