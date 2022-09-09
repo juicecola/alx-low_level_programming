968d6fe0fb9ad1cb60f67b26a292d3547059c7e8
Eric <irerieric@gmail.com>
2022-09-09 12:54:18 -0700

    Numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 int main(void)
 {
   int n;
-  for (n = 0; n <= 10; n++)
+  for (n = 0; n < 10; n++)
   {
     printf("%i", n);
   }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
