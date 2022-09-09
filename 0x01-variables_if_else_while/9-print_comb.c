4d2a2d8b6098a140f82e9fcf94674fb131df00b0
Eric <irerieric@gmail.com>
2022-09-09 13:42:36 -0700

    possible different combinations of two digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,22 +2,20 @@
 
 int main(void)
 {
-	int num1, num2;
+	int num1;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
-		{
+		
 			putchar((num1 % 10) + '0');
-			putchar((num2 % 10) + '0');
-
-			if (num1 == 9 && num2 == 9)
+			
+			if (num1 == 9)
 				continue;
 			
 			putchar(',');
 			putchar(' ');
 		}
-	}
+	
 
 	putchar('\n');
 
