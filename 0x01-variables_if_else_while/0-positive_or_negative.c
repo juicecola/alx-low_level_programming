3f2984bad2195cefa325776a29784f72647b92ef
Eric <irerieric@gmail.com>
2022-09-14 05:38:39 -0700

    prints _putchar

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-/** 
- * main - Entry point
+/**
+* main - Entry point
  * Return Always 0 (Success)
  */
 
@@ -13,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 	        printf("%d is positive\n", n);
 	else if (n < 0)
