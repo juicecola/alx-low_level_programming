c07109f15bc78ad8393fe060eee85ebb65d53d57
Eric <irerieric@gmail.com>
2022-09-15 04:25:11 -0700

    prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,13 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-  
-int a;
-for (a = 'a'; a <= 'z'; a++)
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
 
-  {
-    _putchar(a);
-  }
-_putchar('\n');
+ 		 {		
+   			 _putchar(a);
+ 		 }
+	_putchar('\n');
 }
