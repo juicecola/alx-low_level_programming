a65a9f20ce1fe4ef2116bc1588d2e075c336f7e3
Eric <irerieric@gmail.com>
2022-09-15 07:52:34 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - prints first 50
+ * main - prints sum of even
  * fibonacci numbers
  * Return: return 0
  */
 int main(void)
 {
-	int inc;
-	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
 
-	for (inc = 0; inc < 50; inc++)
+	while (n3 <= 4000000)
 	{
 		n3 = n1 + n2;
-		printf("%lu", n3);
 		n1 = n2;
 		n2 = n3;
 
-		if (inc == 49)
-			printf("\n");
-		else
-			printf(", ");
+		if ((n1 % 2) == 0)
+			sum += n1;
 
 	}
-
+	printf("%ld\n", sum);
 	return (0);
 }
