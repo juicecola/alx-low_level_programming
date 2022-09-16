b181025e716ed0855d06b849c08299f2f73cfb98
Eric <irerieric@gmail.com>
2022-09-16 04:28:16 -0700

    Write a function that prints the numbers, from 0 to 9 Do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all numbers from 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i < 58; i++)
+		if (i != 50 && i != 52)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
