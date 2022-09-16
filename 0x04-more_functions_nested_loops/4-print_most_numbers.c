6aed305eb558a2579e32970abb1caa0d164acf14
Eric <irerieric@gmail.com>
2022-09-16 05:46:00 -0700

    prints the numbers, from 0 to 9 Do not print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,10 @@ void print_numbers(void)
 	int i = 48;
 
 	for (i = 48; i < 58; i++)
-		if (i != 50 && i != 52)
+		if (i == 50 && i == 52)
 	{
-		_putchar(i);
+		continue;
 	}
+	_putchar(i);
 	_putchar('\n');
 }
