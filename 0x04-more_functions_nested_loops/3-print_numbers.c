eac15cafdd8cc2b061c2c757c1e71202e6d2fb1c
Eric <irerieric@gmail.com>
2022-09-16 01:41:40 -0700

    prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints integers
+ *@i: integer
+ * Return: 0 on success
+ *
+ */
+
+void print_numbers(void)
+{
+
+	int i = 48;
+
+	if (i <= 57, i++)
+	{
+	_putchar(i);
+	}
+	_putchar('/n');
+
+}
