5639f8f81b72c60278c029d4d773753695f86792
Eric <irerieric@gmail.com>
2022-10-13 10:23:58 -0700

    function that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * function that prints a name
+ * print_name - function that prints a name
  * @name: name to be printed
- * @f: pointer to the character to be printed
+ * @f: function to be printed
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && *name != '\0' && f != NULL)
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers"
+
+/**
+ * array_iterator - executes a fxn given as parameter on each element of array
+ * @array: given array
+ * @size: size of array
+ * @action: pointer to the function you need to use
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
